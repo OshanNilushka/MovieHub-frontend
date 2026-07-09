1a6a2704265df79ab3962df4b806435ac08420f1
Oshan Nilushka <oshannilushka27@gmail.com>
2026-07-09 23:41:21 +0530

    update it

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'https://moviehub-backend.vercel.app';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://movie-hub-backend-ruby.vercel.app';
